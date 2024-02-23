bb340a7fa90d9fe3df680d3384aaef6a88570642
Caio <108128119+DocCaio@users.noreply.github.com>
2024-02-23 08:57:23 -0300

    up

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,1 +1,29 @@
-import '/Banner.css'
+import './Banner.css';
+
+const Banner = () => {
+    return (
+        <header className='banner'>
+            <nav>
+
+            </nav>
+
+            <h1>
+                What is Carlio and
+                how valid is it ?
+            </h1>            
+            <p>
+            The meaning of production in Carlio is the creation, development, 
+            and the path to progress, and the starting point to achieve
+             the goals that we all have the Petroforce brand, with over 20 years 
+             of experience in the oil and petrochemical industry.
+            </p>
+
+            <a href="#">Buy Products</a>
+            <a href="#">Contact Us</a>
+        </header>
+    
+    )
+}
+
+export default Banner;
+  

b81032d33b6a2bd3c801b6e143638bef43eb2ed2
Caio <108128119+DocCaio@users.noreply.github.com>
2024-02-26 14:21:58 -0300

    up

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,7 +5,40 @@ const About = () => {
 
         <main>
             <section className='about'>
-            <img src="./img/main-car.png" alt="foto de um carro"/>
+            <img className='main-car' src="./img/main-car.png" alt="foto de um carro"/>
+            
+            <div className='about_us'>
+            <img className='component' src="./img/component.svg" alt="foto de um carro"/>
+            <small>About us</small>
+            <h2>Our Company Overview</h2>
+            <p>
+                   Carlio brand is one of the most reliable
+                   motor oil manufacturers, which is engaged
+                   in the production of high quality products with 
+                   a history of more than decades in the industry. 
+                   In order to get more information about other aspects
+                   and products of the Carlio brand,
+                   you can use the following buttons:
+                 </p>
+            </div>
+
+            <div className='links'>
+            <a href="#">Company</a>  
+            <a href="#">Products</a>
+            <a href="#">Our team</a>
+            </div>
+
+            <div>
+                <p>
+                    The meaning of production in Carlio is the creation, development,
+                     and the path to progress, and the starting point to achieve the 
+                     goals that we all have the Petroforce brand, with over 20 years
+                      of experience in the oil and petrochemical industry, we officially
+                       started our activities in the field of design, engineering, construction
+                        of refinery equipment, and the production of various motor and industrial
+                         lubricants in the year 1390 (2011)
+                         </p>
+            </div>
 
             </section>
         </main>

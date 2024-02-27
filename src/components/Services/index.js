7855b34252ac34c1e9e54715b53f8efc66f26352
Caio <108128119+DocCaio@users.noreply.github.com>
2024-02-27 14:57:03 -0300

    up

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -8,11 +8,29 @@ const Services = () => {
                 <div>
                 <img className='box' src="./img/box.svg" alt="foto de um carro"/>
                  <small>Products features</small>
-                </div>
+                
 
-                <div>
+                
                     <h2>The feature of Carlio engine oil that you will benefit from</h2>
                     <a className='visit-btn' href="#">Visit shop page</a>
+                </div>
+
+                <div className='cards'>
+
+                <div  className='cards_benefit'>
+                <img src="./img/star.svg" alt="star"/>
+                <h3>High QualityProduct</h3>
+                </div>
+                <div  className='cards_benefit'> 
+                <img  src="./img/broom.svg" alt="broom"/>
+                <h3>Cleaning of deposits</h3>
+                </div>
+
+                <div  className='cards_benefit'>
+                <img src="./img/security.svg" alt="security shild"/>
+                <h3>Engine protection</h3>
+                </div>
+
                 </div>
                
             </section>

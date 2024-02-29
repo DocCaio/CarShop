c3ee9c66ed2df5003d8441dc676889eb3b96f61b
Caio <108128119+DocCaio@users.noreply.github.com>
2024-02-29 14:13:53 -0300

    up

---
+++ src/components/Brands/index.js
@@ -0,0 +1,21 @@
+import './Brands.css';
+
+const Brands = () => {
+    return (
+
+        <section>
+
+        <span>Brand Cars With Carlio</span>
+
+      <div>
+        <img src="" alt=""></img>
+      </div>
+
+            
+      </section>
+    
+    )
+}
+
+export default Brands;
+  

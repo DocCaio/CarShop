3f71ee01625f13aeb9bb4c6bceb81ad1c6e72016
Caio <108128119+DocCaio@users.noreply.github.com>
2024-03-06 15:05:36 -0300

    uo

--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -24,8 +24,50 @@ const Contacts = () => {
                 <h3>Enter Your Email</h3>
                 <span>Our colleagues will contact you soon</span>
             </span>
-            <input className='submit' type="text"></input>
+            <input className='submit' type="text" placeholder='Enter Email'></input>
+            <img className='arrow'  src="./img/footer/arrow-right.svg" alt="foto de um carro"/>
             </div>
+            <div className='list'>
+                <h4>Product Category</h4>
+                <ul>
+                    <li>
+                   <img  src="./img/footer/arrow-list.svg" alt="foto de um carro"/>
+                    Car Care
+                    </li>
+                    <li> <img  src="./img/footer/arrow-list.svg" alt="foto de um carro"/>Diesel Engine Oil</li>
+                    <li> <img  src="./img/footer/arrow-list.svg" alt="foto de um carro"/>Gear</li>
+                    <li> <img  src="./img/footer/arrow-list.svg" alt="foto de um carro"/>Motorbike</li>
+                    <li> <img  src="./img/footer/arrow-list.svg" alt="foto de um carro"/>More ...</li>
+                </ul>
+            </div>
+            <div className='list'>
+                <h4>Useful links</h4>
+                <ul>
+                    <li> <img  src="./img/footer/arrow-list.svg" alt="foto de um carro"/>Home</li>
+                    <li> <img  src="./img/footer/arrow-list.svg" alt="foto de um carro"/>Products & Shopl</li>
+                    <li> <img  src="./img/footer/arrow-list.svg" alt="foto de um carro"/>Pictures & Stories</li>
+                    <li> <img  src="./img/footer/arrow-list.svg" alt="foto de um carro"/>Contact us & Support</li>
+                    <li> <img  src="./img/footer/arrow-list.svg" alt="foto de um carro"/>Blog Page</li>
+                </ul>
+            </div>
+
+            <div className='list'>
+                <h4>Contact Us</h4>
+                <h5>Dubai Address</h5>
+                <span>
+                    Churchill Executive Tower, Business Bay, 
+                    Dubai, UAE P.O.Box: 28824 Dubai.UAE
+                </span>
+                <h5>Istanbul Address</h5>
+                <span>
+                University neighborhood, Civan Alley, No:1,
+                 Allure tower, unit:384 Avcilar/Istanbul
+                </span>               
+            </div>
+            <div>
+                <p>©| All Rights Reserved | Powered by Caio Design Department</p>
+            </div>
+            <img className='photo'  src="./img/footer/copy-right.svg" alt="foto de um carro"/>
             </section>    
         </footer>
     )
